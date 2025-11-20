e5c4d783955d88cb7dd4957223cc0f2924749d31
carlosguachalla <carlos.guachalla@ucb.edu.bo>
2025-11-20 17:49:15 -0400

    Add files via upload

---
+++ ProgramcionPrincipal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <libbelisuarius.h>
+using namespace std;
+int main(){
+    cout<<"La suma entre esos numeros es: "<<Suma(4,3)<<endl;
+    cout<<"La resta entre esos numeros es: "<<Resta(4,3)<<endl;
+    cout<<"La division entre esos numeros es: "<<Division(4,3)<<endl;
+    cout<<"La multiplicion entre esos numeros es: "<<Multiplicacion(4,3)<<endl;
+}
